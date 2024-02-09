528635082f08093a653a475502b74774b757e0c2
SaadM33 <smaaqili@gmail.com>
2024-02-09 16:18:13 +0100

    completed the file base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ contains the base class"""
 
+import models
 import uuid
 from datetime import datetime
 
@@ -17,14 +18,22 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
         if kwargs:
-            pass
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
     
      
     def save(self):
         """update updated_at variable with the current date"""
         self.updated_at = datetime.now()
-        
+        models.storage.save()
+
+
     def to_dict(self):
         """Returns a dictionary representation of self"""
         obj_dict = self.__dict__.copy()
@@ -32,6 +41,7 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
+
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
