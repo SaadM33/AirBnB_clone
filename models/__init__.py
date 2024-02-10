01255b729b9ed190cc06479026453d27c724af3b
SaadM33 <smaaqili@gmail.com>
2024-02-10 19:04:37 +0100

    fixed import error

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 """__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
+import os
+import sys
+
+# Get the current script's directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Append the parent directory to the sys.path
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+grand_parent_dir = os.path.abspath(os.path.join(parent_dir, '..'))
+sys.path.extend([parent_dir, grand_parent_dir])
 
 
 storage = FileStorage()
