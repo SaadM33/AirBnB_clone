e50704514f0723ed28426dd55533253dde0785c4
SaadM33 <smaaqili@gmail.com>
2024-02-10 04:31:26 +0100

    added to console and storage

--- console.py
+++ console.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""the entry point of the command interpreter"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+        """Defines the HolbertonBnB command interpreter."""
+        
+
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines the FileStorage class."""
+
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -9,3 +10,42 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
+
+class FileStorage:
+    """Represent an abstracted storage engine.
+
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
+
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary of serialized objects."""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Adds a new object to the dictionary of serialized objects."""
+        FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for obj in objdict.values():
+                    cls_name = obj["__class__"]
+                    del obj["__class__"]
+                    self.new(eval(cls_name)(**obj))
+        except FileNotFoundError:
+            pass
+
+    def save(self):
+        """Serializes the dictionary of objects to the JSON file.(after converting it to a dict)"""
+        object_dict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(object_dict, f)
+
+    
