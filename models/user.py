675d708550ee4c6c3b0516e504a5e81d75b1a104
SaadM33 <smaaqili@gmail.com>
2024-02-10 20:00:12 +0100

    refixed import error hallucination

--- models/user.py
+++ models/user.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
 """Defines the User class."""
-import os
-import sys
-
-# Get the current script's directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Append the parent directory to the sys.path
-parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
-grand_parent_dir = os.path.abspath(os.path.join(parent_dir, '..'))
-sys.path.extend([parent_dir, grand_parent_dir])
-
 from models.base_model import BaseModel
 
 class User(BaseModel):
