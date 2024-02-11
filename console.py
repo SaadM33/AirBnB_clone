2b856df9b709e857cadfc67d53beae25f1a19eae
SaadM33 <smaaqili@gmail.com>
2024-02-11 02:58:29 +0100

    task 6

--- console.py
+++ console.py
@@ -2,10 +2,45 @@
 """the entry point of the command interpreter"""
 
 import cmd
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
+
+__classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
 
 class HBNBCommand(cmd.Cmd):
     """Defines the HolbertonBnB command interpreter."""
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """Do nothing when receiving an empty line."""
+        return
+
+    def do_quit(self):
+        """Exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program"""
+        print()
+        return True
+
+
+
 
 
 if __name__ == '__main__':
